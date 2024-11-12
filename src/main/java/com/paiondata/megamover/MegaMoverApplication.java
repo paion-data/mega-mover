3dd4e77e549a0b1eb466a18e75ab28ac3c09197e
热斗游侠 <1633486422@qq.com>
2024-11-12 19:52:41 +0800

    Initial commit

---
+++ src/main/java/com/paiondata/megamover/MegaMoverApplication.java
@@ -0,0 +1,13 @@
+package com.paiondata.megamover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaMoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegaMoverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paiondata/megamover/MegaMoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paiondata.megamover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaMoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
